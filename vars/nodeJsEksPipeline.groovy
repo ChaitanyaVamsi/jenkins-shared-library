4124d6458b63c10c802059c5c14092f5549db02c
chaitanya <vamsi.blaze1@gmail.com>
2026-01-08 20:35:21 +0530

    updated

--- vars/nodeJsEksPipeline.groovy
+++ vars/nodeJsEksPipeline.groovy
@@ -166,7 +166,7 @@ def call(Map config){
             stage('Trigger Dev catalogue-deploy'){
               steps{
                 script{
-                  build job:'catalogue-deploy'
+                  build job:'catalogue-deploy',
                   wait: false,
                   propogate: false
                 }
